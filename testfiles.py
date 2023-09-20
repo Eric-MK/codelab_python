f3972471d8e84c0e459060a68158a3ac3846a693
unknown <mutungaeric839@gmail.com>
2023-09-20 22:17:11 +0300

    file path working

--- testfiles.py
+++ testfiles.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Replace 'your_file.xlsx' with the path to your Excel file
-file_path = 'your_file.xlsx'
+file_path = 'C:\\Users\\Eric\\Desktop\\graphics\\Test Files.xlsx'
 
 # Read the Excel file into a DataFrame
 df = pd.read_excel(file_path)
