f1fd1b922412f2d165c54bb2eef883dfa30afd39
unknown <mutungaeric839@gmail.com>
2023-09-24 14:16:59 +0300

    email structure working

--- testfiles.py
+++ testfiles.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import re  # Import the regular expressions library
+
 
 # Specify the absolute path to your Excel file
 file_path = r'C:\Users\Eric\Desktop\graphics\Test Files.xlsx'
@@ -18,23 +20,34 @@ email_domain = "gmail.com"
 
 # Define a function to generate email addresses
 def generate_email(student_name, email_domain):
-    # Split the student name by commas and spaces
-    name_parts = student_name.split(', ')
+    # Remove special characters and spaces from the student_name
+    cleaned_name = re.sub(r'[^a-zA-Z0-9 ]', '', student_name)
+
+    # Split the cleaned name by spaces
+    name_parts = cleaned_name.split()
     
-    # Extract the first and last names
-    if len(name_parts) == 2:
-        last_name, first_name = name_parts
-    else:
-        first_name = name_parts[0]
-        last_name = ""
+    # Initialize variables for last name, first name, middle name, and forth name
+    last_name = ""
+    first_name = ""
+    middle_name = ""
+    forth_name = ""
     
+    # Check the number of name parts and assign them accordingly
+    if len(name_parts) >= 1:
+        last_name = name_parts[0]
+    if len(name_parts) >= 2:
+        middle_name = name_parts[1]
+    if len(name_parts) >= 3:
+        middle_name = name_parts[2]
+    if len(name_parts) >= 4:
+        forth_name = name_parts[3]
+
     # Take the first letter of the first name and concatenate it with the last name
-    email = f"{last_name[0].lower()}{first_name.lower()}@{email_domain}"
+    email = f"{last_name.lower()[0]}{middle_name.lower()}@{email_domain}"
     
     return email
-
 # Apply the generate_email function to create email addresses
 df['Email Address'] = df['Student Name'].apply(lambda x: generate_email(x, email_domain))
 
 # Display the DataFrame with the newly generated email addresses
-print(df[['Student Name', 'Email Address']])
+print(df)
